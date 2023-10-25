5d1a4b14c8e4ce7ebb31dd0ecc5846f83b208fc7
swoody2527 <swoodall2527@gmail.com>
2023-10-25 14:12:22 +0100

    added filter for articles

---
+++ nc-news-fe/src/components/TopicFilter.jsx
@@ -0,0 +1,48 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+function TopicFilter() {
+  const [topics, setTopics] = useState([]);
+  const [topicQuery, setTopicQuery] = useState("");
+
+  useEffect(() => {
+    axios
+      .get("https://be-nc-news-sopv.onrender.com/api/topics")
+      .then((response) => {
+        setTopics(response.data.topics);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <select
+        name="topic"
+        id="topic"
+        onChange={(e) => setTopicQuery(e.target.value)}>
+        <option value={""}>All</option>
+        {topics.map((topic) => {
+          return (
+            <option value={topic.slug} key={topic.slug}>
+              {topic.slug}
+            </option>
+          );
+        })}
+      </select>
+      <Link
+        to={{
+          pathname: "/articles",
+          search: topicQuery ? `?topic=${topicQuery}` : "",
+        }}>
+        <button type="submit">Search</button>
+      </Link>
+    </form>
+  );
+}
+
+export default TopicFilter;
